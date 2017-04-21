ace243e98cd719256870d085b89e7f253fe8a66f
Saar Shen <saars@microsoft.com>
2017-04-21 14:18:11 -0700

    Feature/dual heads (#4)
    * Set a base for dual headers
    
    * Resove the build
    
    * Address a warning

--- src/ApplicationInsights.ServiceFabric/Net45/FabricTelemetryInitializer.cs
+++ src/ApplicationInsights.ServiceFabric/Net45/FabricTelemetryInitializer.cs
@@ -1,73 +1,24 @@
 ﻿namespace Microsoft.ApplicationInsights.ServiceFabric
 {
-    using System;
-	using System.Collections.Generic;
-#if !NETCORE
+    using System.Collections.Generic;
     using System.Fabric;
-#endif
     using System.Globalization;
     using System.Runtime.Remoting.Messaging;
-	using System.Text;
-    using Microsoft.ApplicationInsights.Channel;
     using Microsoft.ApplicationInsights.Extensibility;
 
-    /// <summary>
-    /// Telemetry initializer for service fabric. Adds service fabric specific context to outgoing telemetry.
-    /// </summary>
-    public class FabricTelemetryInitializer : ITelemetryInitializer
+    public partial class FabricTelemetryInitializer
     {
-        private const string ServiceContextKeyName = "AI.SF.ServiceContext";
-
         private Dictionary<string, string> contextCollection;
 
-        /// <summary>
-        /// There are a few ways the context could be provided. This property makes it easy for the rest of the implemenatation to ignore all those cases. 
-        /// </summary>
-        private Dictionary<string, string> ApplicableServiceContext
-        {
-            get
-            {
-                if (this.contextCollection != null)
-                {
-                    return this.contextCollection;
-                }
-
-                return CallContext.LogicalGetData(ServiceContextKeyName) as Dictionary<string, string>;
-                //if (fromCallContext != null)
-                //{
-                //    return fromCallContext;
-                //}
-            }
-        }
-
-#if !NETCORE
         /// <summary>
         /// Initializes a new instance of the <see cref="FabricTelemetryInitializer"/> class.
         /// </summary>
         /// <param name="context">a service context object.</param>
         public FabricTelemetryInitializer(ServiceContext context)
         {
-            // Clone the originally passed in dictionary entries. We don't want to hold the original dictionary reference because the user may add / remove from it etc. and that would result in weird changes.
             this.contextCollection = GetContextContractDictionaryFromServiceContext(context);
         }
-#endif
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FabricTelemetryInitializer"/> class.
-        /// </summary>
-        public FabricTelemetryInitializer()
-        {}
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FabricTelemetryInitializer"/> class.
-        /// </summary>
-        public FabricTelemetryInitializer(Dictionary<string, string> context)
-        {
-            // Clone the passed in context.
-            this.contextCollection = new Dictionary<string, string>(context);
-        }
-
-#if !NETCORE
         /// <summary>
         /// This static method is a helper method that anyone can invoke to set the call context.
         /// This provides a way for the user to add a single line of code at the entry point and get collected telemetry augmented with service fabric specific fields.
@@ -80,78 +31,13 @@
 
             CallContext.LogicalSetData(ServiceContextKeyName, GetContextContractDictionaryFromServiceContext(context));
         }
-#endif
-
-        /// <summary>
-        /// Adds service fabric context fields on the given telemetry object.
-        /// </summary>
-        /// <param name="telemetry">The telemetry item being sent through the AI sdk.</param>
-        public void Initialize(ITelemetry telemetry)
-        {
-            try
-            {
-                if (this.ApplicableServiceContext != null)
-                {
-                    foreach (var field in this.ApplicableServiceContext)
-                    {
-                        if (!telemetry.Context.Properties.ContainsKey(field.Key))
-                        {
-                            telemetry.Context.Properties.Add(field.Key, field.Value);
-                        }
-                    }
-
-                    if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName) && this.ApplicableServiceContext.ContainsKey(KnownContextFieldNames.ServiceName))
-                    {
-                        telemetry.Context.Cloud.RoleName = this.ApplicableServiceContext[KnownContextFieldNames.ServiceName];
-                    }
-                    if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleInstance))
-                    {
-                        if (this.ApplicableServiceContext.ContainsKey(KnownContextFieldNames.InstanceId))
-                        {
-                            telemetry.Context.Cloud.RoleInstance = this.ApplicableServiceContext[KnownContextFieldNames.InstanceId];
-                        }
-                        else if (this.ApplicableServiceContext.ContainsKey(KnownContextFieldNames.ReplicaId))
-                        {
-                            telemetry.Context.Cloud.RoleInstance = this.ApplicableServiceContext[KnownContextFieldNames.ReplicaId];
-                        }
-                    }
-                }
-
-                // Fallback to environment variables for setting role / instance names. We will rely on these environment variables exclusively for container lift and shift scenarios for now.
-                // And for reliable services, when service context is neither provided directly nor through call context
-                if(string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
-                {
-                    telemetry.Context.Cloud.RoleName = Environment.GetEnvironmentVariable(KnownEnvironmentVariableName.ServicePackageName);
-                }
-                
-                if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleInstance))
-                {
-                    telemetry.Context.Cloud.RoleInstance = Environment.GetEnvironmentVariable(KnownEnvironmentVariableName.ServicePackageActivatonId) ?? Environment.GetEnvironmentVariable(KnownEnvironmentVariableName.ServicePackageInstanceId);
-                }
-
-                if (!telemetry.Context.Properties.ContainsKey(KnownContextFieldNames.NodeName))
-                {
-                    string nodeName = Environment.GetEnvironmentVariable(KnownEnvironmentVariableName.NodeName);
-
-                    if (!string.IsNullOrEmpty(nodeName))
-                    {
-                        telemetry.Context.Properties.Add(KnownContextFieldNames.NodeName, nodeName);
-                    }
-                }
-            }
-            catch
-            {
-                // Something went wrong trying to set these extra properties. We shouldn't fail though.
-            }
-        }
-
-#if !NETCORE
+        
         /// <summary>
         /// Converts the context object to the loose dictionary based contract this initializer depends on for data.
         /// </summary>
         /// <param name="context">An object of type <see cref="ServiceContext" />.</param>
         /// <returns>A dictionary that encapsulates the given context.</returns>
-        private static Dictionary<string,string> GetContextContractDictionaryFromServiceContext(ServiceContext context)
+        private static Dictionary<string, string> GetContextContractDictionaryFromServiceContext(ServiceContext context)
         {
             Dictionary<string, string> result = new Dictionary<string, string>();
 
@@ -176,27 +62,5 @@
 
             return result;
         }
-#endif
-
-        private class KnownContextFieldNames
-        {
-            public const string ServiceName = "ServiceFabric.ServiceName";
-            public const string ServiceTypeName = "ServiceFabric.ServiceTypeName";
-            public const string PartitionId = "ServiceFabric.PartitionId";
-            public const string ApplicationName = "ServiceFabric.ApplicationName";
-            public const string ApplicationTypeName = "ServiceFabric.ApplicationTypeName";
-            public const string NodeName = "ServiceFabric.NodeName";
-            public const string InstanceId = "ServiceFabric.InstanceId";
-            public const string ReplicaId = "ServiceFabric.ReplicaId";
-        }
-
-        private class KnownEnvironmentVariableName
-        {
-            public const string ServicePackageName = "Fabric_ServicePackageName";
-            public const string ServicePackageInstanceId = "Fabric_ServicePackageInstanceId";
-            public const string ServicePackageActivatonId = "Fabric_ServicePackageActivationId";
-            public const string NodeName = "Fabric_NodeName";
-        }
     }
 }
-
