b5e68fad5cb0d294a2c72812cad8f45e02156bb8
mikanyg <mikael@mny.dk>
2017-11-08 21:30:08 +0100

    Added new telemetry initializer that sets component version of a telemetry item based on the code package version of a reliable service, if it has not already been set by other means.

---
+++ src/ApplicationInsights.ServiceFabric.Native/Net45/CodePackageVersionTelemetryInitializer.cs
@@ -0,0 +1,31 @@
+﻿namespace Microsoft.ApplicationInsights.ServiceFabric
+{
+    using System;
+    using System.Fabric;
+    using Microsoft.ApplicationInsights.Channel;
+    using Microsoft.ApplicationInsights.Extensibility;
+
+    /// <summary>
+    /// A telemetry initializer that will set component version based on the code package version of a Service Fabric Reliable Service.
+    /// </summary>
+    public class CodePackageVersionTelemetryInitializer : ITelemetryInitializer
+    {
+        /// <summary>
+        /// Initializes component version of the telemetry item with the code package version from the Service Fabric runtime activation context.
+        /// </summary>
+        /// <param name="telemetry">The telemetry context to initialize.</param>
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (telemetry == null)
+            {
+                throw new ArgumentNullException(nameof(telemetry));
+            }
+
+            if (telemetry.Context?.Component != null && string.IsNullOrEmpty(telemetry.Context.Component.Version))
+            {
+                var activationContext = FabricRuntime.GetActivationContext();
+                telemetry.Context.Component.Version = activationContext.CodePackageVersion;
+            }
+        }
+    }
+}
